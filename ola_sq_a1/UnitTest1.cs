37e38fe243dc7b56a06102737d37e03e4db6a763
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2024-09-04 15:56:10 +0200

    Adds tests and fix to csproj to specify main method

--- ola_sq_a1/UnitTest1.cs
+++ ola_sq_a1/UnitTest1.cs
@@ -5,6 +5,53 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        //Test CreateTask
+        TaskFacade taskFacade = new TaskFacade();
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        Assert.Equal("Test", task.Description);
+        Assert.Equal(now, task.Deadline);
+        Assert.False(task.IsFinished);
+        Assert.Equal("Test", task.Category);
     }
+
+    [Fact]
+    public void Test2()
+    {
+        //Test UpdateTask
+        TaskFacade taskFacade = new TaskFacade();
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        task = taskFacade.UpdateTask(task, "Test2", now, true, "Test2");
+        Assert.Equal("Test2", task.Description);
+        Assert.Equal(now, task.Deadline);
+        Assert.True(task.IsFinished);
+        Assert.Equal("Test2", task.Category);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        //Test MarkAsFinished
+        TaskFacade taskFacade = new TaskFacade();
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, false, "Test");
+        task = taskFacade.MarkAsFinished(task);
+        Assert.True(task.IsFinished);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        //Test MarkAsUnfinished
+        TaskFacade taskFacade = new TaskFacade();
+        DateTime now = DateTime.Now;
+        Task task = taskFacade.CreateTask("Test", now, true, "Test");
+        task = taskFacade.MarkAsUnfinished(task);
+        Assert.False(task.IsFinished);
+    }
+
+
+
+
 }
--- ola_sq_a1/obj/Debug/net8.0/ola_sq_a1.AssemblyInfo.cs
+++ ola_sq_a1/obj/Debug/net8.0/ola_sq_a1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ola_sq_a1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+848b3e1b9dc8607be98cbe7e949d2ba624b16d74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed237c77571b02487a0d65ab809e421928a629bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ola_sq_a1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ola_sq_a1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
